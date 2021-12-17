95c17e15bfde79826fc72721b0c477d880460406
Torie <38877217+Torietron@users.noreply.github.com>
2021-12-17 15:56:53 -0500

    adding basic window functionality

--- main.cpp
+++ main.cpp
@@ -1,25 +1,42 @@
 #include "DxLib.h"
 #include <cstdint>
 
+uint_fast8_t WinMode = FALSE;
+int_fast16_t Width = 640, Height = 480;
+
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 				 LPSTR lpCmdLine, int nCmdShow )
 {
+	//window init
+	ChangeWindowMode(WinMode);
+    SetWindowSizeChangeEnableFlag(TRUE, TRUE);
+    SetWindowSizeExtendRate(1.0);
+	SetMainWindowText("Test");
+	SetGraphMode(Width, Height, 16);
 
-	SetGraphMode(640, 480, 16);
-
+	//check DXlibrary
 	if( DxLib_Init() == -1 ) return -1;
 	
 	//preload setup
 	SetDrawScreen(DX_SCREEN_BACK);
 
+	//main loop
 	while(1)
 	{
 	    ClearDrawScreen();
 		{
-
+			LoadGraphScreen(Width/2,Height/2-20,_T("core/ph0.png"),TRUE);
 		}
+
 		ScreenFlip();
+
 		if(ProcessMessage() < 0 ) break;
+		if(CheckHitKey(KEY_INPUT_RALT) == 1 && CheckHitKey(KEY_INPUT_RETURN) == 1) 
+		{
+			if(WinMode == FALSE) WinMode = TRUE;
+			else WinMode = FALSE;
+			ChangeWindowMode(WinMode);
+		}
 		if(CheckHitKey(KEY_INPUT_ESCAPE)) break;
 	}
 	DxLib_End();
