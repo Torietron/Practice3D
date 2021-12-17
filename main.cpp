dedc99a39fabcef26dbf73faab62d8f845727d61
Torie <38877217+Torietron@users.noreply.github.com>
2021-12-17 18:02:45 -0500

    screen reload and camera testing
    Signed-off-by: Torie <38877217+Torietron@users.noreply.github.com>
    Signed-off-by: Torie <Torietron@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
+#define _USE_MATH_DEFINES
 #include "DxLib.h"
 #include <cstdint>
+#include <cmath>
 
-uint_fast8_t WinMode = FALSE;
+static const float ROTATE_SPEED = DX_PI_F/90;
+uint_fast8_t WinMode = TRUE, NewScreen = TRUE;
 int_fast16_t Width = 640, Height = 480;
+
+void rotate(float &x, float &y, const float angle, const float mikuX, const float mikuY);
 
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 				 LPSTR lpCmdLine, int nCmdShow )
@@ -19,6 +24,11 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	
 	//preload setup
 	SetDrawScreen(DX_SCREEN_BACK);
+	int ModelH;
+	NewScreen = TRUE;
+	
+	float cameraX = 0, cameraZ =-20;
+	const float targetX = 0, targetZ = 0;
 
 	//main loop
 	while(ProcessMessage() >= 0) //check app's process state
@@ -27,12 +37,22 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		{
 			//Update
 			{
-				//code
+				if(NewScreen == TRUE)
+				{
+					ModelH = MV1LoadModel("dat/Lat/LatMikuVer2.3_Normal.pmd");
+					SetCameraNearFar(0.1f, 1000.0f);
+					SetCameraPositionAndTarget_UpVecY(VGet(0, 10, -20),VGet(0.0f, 10.0f, 0.0f));
+					NewScreen = FALSE;
+				}
+
+				if(CheckHitKey(KEY_INPUT_LEFT) == 1) rotate(cameraX, cameraZ, +ROTATE_SPEED, targetX, targetZ);
+				if(CheckHitKey(KEY_INPUT_RIGHT) == 1) rotate(cameraX, cameraZ, -ROTATE_SPEED, targetX, targetZ);
+				SetCameraPositionAndTarget_UpVecY(VGet(cameraX, 10, cameraZ), VGet(targetX, 10.0f, targetZ));
 			}
 
 			//Draw
 			{
-				LoadGraphScreen(Width/2,Height/2-20,_T("core/ph0.png"),TRUE);
+				MV1DrawModel(ModelH);
 			}
 		}
 		ScreenFlip();
@@ -43,9 +63,20 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 			if(WinMode == FALSE) WinMode = TRUE;
 			else WinMode = FALSE;
 			ChangeWindowMode(WinMode);
+			NewScreen = TRUE;
 		}
 		if(CheckHitKey(KEY_INPUT_ESCAPE)) break;
 	}
 	DxLib_End();
 	return 0;					
+}
+
+void rotate(float &x, float &y, const float angle, const float tX, const float tY)
+{
+	float ox = x-tX;
+	float oy = y-tY;
+	x = ox * cos(angle) + oy * sin(angle);
+	y = ox * sin(angle) + oy * cos(angle);
+	x += tX;
+	y += tY;
 }
