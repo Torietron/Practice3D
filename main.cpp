0b75f9e47d0a53de99f52b9448f25421e41e3a36
Torie <38877217+Torietron@users.noreply.github.com>
2021-12-18 02:11:57 -0500

    added idle animation and more controls

--- main.cpp
+++ main.cpp
@@ -4,18 +4,19 @@
 #include <cmath>
 
 static const float ROTATE_SPEED = DX_PI_F/90;
+static const float MOVEMENT_SPEED = DX_PI_F/50;
 uint_fast8_t WinMode = TRUE, NewScreen = TRUE;
 int_fast16_t Width = 640, Height = 480;
 
-void rotate(float &x, float &y, const float angle, const float mikuX, const float mikuY);
+void rotate(float &x, float &y, const float angle, const float tX, const float tY);
 
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 				 LPSTR lpCmdLine, int nCmdShow )
 {
 	//window init
 	ChangeWindowMode(WinMode);
-    	SetWindowSizeChangeEnableFlag(TRUE, TRUE);
-    	SetWindowSizeExtendRate(1.0);
+	SetWindowSizeChangeEnableFlag(TRUE, TRUE);
+	SetWindowSizeExtendRate(1.0);
 	SetMainWindowText("Test");
 	SetGraphMode(Width, Height, 16);
 
@@ -24,11 +25,14 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	
 	//preload setup
 	SetDrawScreen(DX_SCREEN_BACK);
-	int ModelH;
 	NewScreen = TRUE;
-	
-	float cameraX = 0, cameraZ =-20;
-	const float targetX = 0, targetZ = 0;
+	uint_fast8_t pDir = 0, AnimSet = 0;
+	int ModelH;
+	int AttachIndex;
+	float TotalTime, PlayTime = 0.0f;
+	float playerX = 0.0f, playerY = 0.0f, playerZ = 0.0f, rotX = 0.0f, rotY = DX_PI_F/3, rotZ = 0.0f;
+	float cameraX = 0.0f, cameraY = 10.0f, cameraZ =-20.0f;
+	const float targetX = 0.0f, targetY = 10.0f, targetZ = 0.0f;
 
 	//main loop
 	while(ProcessMessage() >= 0) //check app's process state
@@ -37,17 +41,51 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		{
 			//Update
 			{
+				//on screen event
 				if(NewScreen == TRUE)
 				{
 					ModelH = MV1LoadModel("dat/Lat/LatMikuVer2.3_Normal.pmd");
 					SetCameraNearFar(0.1f, 1000.0f);
-					SetCameraPositionAndTarget_UpVecY(VGet(0, 10, -20),VGet(0.0f, 10.0f, 0.0f));
+					SetCameraPositionAndTarget_UpVecY(VGet(0.0f, 10.0f, -20.0f),VGet(0.0f, 10.0f, 0.0f));
+					AttachIndex = MV1AttachAnim(ModelH, 0, -1, FALSE);
+					TotalTime = MV1GetAttachAnimTotalTime(ModelH,AttachIndex);
 					NewScreen = FALSE;
 				}
 
-				if(CheckHitKey(KEY_INPUT_LEFT) == 1) rotate(cameraX, cameraZ, +ROTATE_SPEED, targetX, targetZ);
-				if(CheckHitKey(KEY_INPUT_RIGHT) == 1) rotate(cameraX, cameraZ, -ROTATE_SPEED, targetX, targetZ);
-				SetCameraPositionAndTarget_UpVecY(VGet(cameraX, 10, cameraZ), VGet(targetX, 10.0f, targetZ));
+				//frame
+				PlayTime += 0.27f;
+				if(PlayTime >= TotalTime) PlayTime = 0.0f;
+				
+				//controls
+				if(CheckHitKey(KEY_INPUT_A) == 1) rotY -= ROTATE_SPEED;
+				if(CheckHitKey(KEY_INPUT_D) == 1) rotY += ROTATE_SPEED;
+				if(CheckHitKey(KEY_INPUT_W) == 1) 
+				{
+				playerZ -= cos(rotY)*MOVEMENT_SPEED;
+				playerX -= sin(rotY)*MOVEMENT_SPEED;
+				//pDir = 1;
+				}
+				if(CheckHitKey(KEY_INPUT_S) == 1) 
+				{
+				playerZ += cos(rotY)*MOVEMENT_SPEED;
+				playerX += sin(rotY)*MOVEMENT_SPEED;
+				//pDir = 1;
+				}
+				//if(CheckHitKey(KEY_INPUT_W) == 0 && CheckHitKey(KEY_INPUT_S) == 0) pDir = 0;
+				//if(CheckHitKey(KEY_INPUT_SPACE) == 1) pDir = 0;
+				
+				if(AnimSet != pDir)
+				{
+					AttachIndex = MV1AttachAnim(ModelH, pDir, -1, FALSE);
+					TotalTime = MV1GetAttachAnimTotalTime(ModelH,AttachIndex);
+					PlayTime = 0.0f;
+					AnimSet = pDir; 
+				}
+				MV1SetAttachAnimTime(ModelH,AttachIndex,PlayTime);
+				MV1SetPosition(ModelH,VGet(playerX,playerY,playerZ));
+				MV1SetRotationXYZ(ModelH,VGet(0.0f,rotY,0.0f));
+				//SetCameraPositionAndTarget_UpVecY(VGet(cameraX, cameraY, cameraZ), VGet(targetX, targetY, targetZ));
+				DrawFormatString(0,20,GetColor(255,255,255),"x=%.1f y=%.1f z=%.1f",playerX,playerY,playerZ);
 			}
 
 			//Draw
