3c7a90d88a3fe751d22e6f79f066925a2e825c87
Torie <38877217+Torietron@users.noreply.github.com>
2021-12-20 23:53:56 -0500

    added camera functionality, cleaned data
    Signed-off-by: Torie <38877217+Torietron@users.noreply.github.com>
    Signed-off-by: Torie <Torietron@gmail.com>

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ static const float MOVEMENT_SPEED = DX_PI_F/10;
 uint_fast8_t WinMode = TRUE, NewScreen = TRUE;
 int_fast16_t Width = 640, Height = 480;
 
-void rotate(float &x, float &y, const float angle, const float tX, const float tY);
+void pRotate(float &x, float &y, const float angle, const float tX, const float tY);
 
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 				 LPSTR lpCmdLine, int nCmdShow )
@@ -27,14 +27,19 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	SetDrawScreen(DX_SCREEN_BACK);
 	NewScreen = TRUE;
 	uint_fast8_t pDir = 0, AnimSet = 0, pReverse = FALSE, pJump = FALSE, pGrounded = TRUE;
+	int_fast16_t tempX, tempY;
 	int_fast32_t pPace = 0;
-	int ModelH;
+	int ModelH, EnvH1, EnvH2, SkyH;
 	int AttachIndex;
 	float TotalTime, PlayTime = 0.0f;
-	float playerX = 0.0f, playerY = 0.0f, playerZ = 0.0f, anchorY = 0.0f; 
-	float rotX = 0.0f, rotY = DX_PI_F/3, rotZ = 0.0f;
-	float cameraX = 0.0f, cameraY = 10.0f, cameraZ =-20.0f;
-	const float targetX = 0.0f, targetY = 10.0f, targetZ = 0.0f;
+	float anchorY = 0.0f, angleV, angleH; 
+	float targetX = 0.0f, targetY = 10.0f, targetZ = 0.0f;
+	VECTOR Camera, Player, pRot, cRot, S;
+	Camera = VGet(0.0f, 20.0f, -20.0f);
+	Player = VGet(0.0f, 0.0f, 0.0f);
+	pRot = VGet(0.0f, DX_PI_F/3, 0.0f);
+	cRot = VGet(0.0f, DX_PI_F/3, 0.0f);
+	SetMousePoint((int)Player.x,(int)Player.z);
 
 	//Main Loop
 	while(ProcessMessage() >= 0) //Check app's process state
@@ -47,8 +52,11 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 				if(NewScreen == TRUE)
 				{
 					ModelH = MV1LoadModel(_T("dat/Lat/LatMikuVer2.3_Normal.pmd"));
+					EnvH1 = MV1LoadModel(_T("dat/batokin-island/batokin_island5.mqo"));
+					//EnvH2 = MV1LoadModel(_T("dat/batokin-island/batokin_island5.x"));
+					SkyH = MV1LoadModel(_T("dat/batokin-island/skydome.x"));
 					SetCameraNearFar(0.1f, 1000.0f);
-					SetCameraPositionAndTarget_UpVecY(VGet(0.0f, 10.0f, -20.0f),VGet(0.0f, 10.0f, 0.0f));
+					SetCameraPositionAndTarget_UpVecY(VGet(Camera.x, Camera.y, Camera.z),VGet(targetX, targetY, targetZ));
 					AttachIndex = MV1AttachAnim(ModelH, 0, -1, FALSE);
 					TotalTime = MV1GetAttachAnimTotalTime(ModelH,AttachIndex);
 					NewScreen = FALSE;
@@ -56,22 +64,22 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 				
 				//Controls
 				{
-					if(CheckHitKey(KEY_INPUT_A) == 1) rotY -= ROTATE_SPEED;
-					if(CheckHitKey(KEY_INPUT_D) == 1) rotY += ROTATE_SPEED;
+					if(CheckHitKey(KEY_INPUT_A) == 1) pRot.y -= ROTATE_SPEED;
+					if(CheckHitKey(KEY_INPUT_D) == 1) pRot.y += ROTATE_SPEED;
 					if(CheckHitKey(KEY_INPUT_W) == 1) 
 					{
-						playerZ -= cos(rotY)*MOVEMENT_SPEED;
-						playerX -= sin(rotY)*MOVEMENT_SPEED;
-						rotX = DX_PI_F*2;
+						Player.z -= cos(pRot.y)*MOVEMENT_SPEED;
+						Player.x -= sin(pRot.y)*MOVEMENT_SPEED;
+						pRot.x = DX_PI_F*2;
 						if(pGrounded == TRUE) pDir = 1;
 						else pDir = 2;
 						pReverse = FALSE;
 					}
 					if(CheckHitKey(KEY_INPUT_S) == 1) 
 					{
-						playerZ += cos(rotY)*(MOVEMENT_SPEED*0.75f);
-						playerX += sin(rotY)*(MOVEMENT_SPEED*0.75f);
-						rotX = DX_PI_F/10; //Center of gravity would've been visually inconsistent
+						Player.z += cos(pRot.y)*(MOVEMENT_SPEED*0.75f);
+						Player.x += sin(pRot.y)*(MOVEMENT_SPEED*0.75f);
+						pRot.x = DX_PI_F/10; //Center of gravity would've been visually inconsistent
 						if(pGrounded == TRUE) pDir = 1;
 						else pDir = 2;
 						pReverse = TRUE;
@@ -80,27 +88,43 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 					{
 						if(pGrounded == TRUE) 
 						{
-							anchorY = playerY;
+							anchorY = Player.y;
 							pPace = 0;
 							pJump = TRUE;
 							pGrounded = FALSE;
-							rotX = DX_PI_F*2;
+							pRot.x = DX_PI_F*2;
 							pDir = 2;
 						}
 					}
 					if(CheckHitKey(KEY_INPUT_W) == 0 && CheckHitKey(KEY_INPUT_S) == 0)
 					{
-						if(pGrounded == TRUE) pDir = 0, rotX = DX_PI_F*2, pReverse = FALSE;
-						if(pGrounded == FALSE) pDir = 2, rotX = DX_PI_F*2, pReverse = FALSE;
+						if(pGrounded == TRUE) pDir = 0, pRot.x = DX_PI_F*2, pReverse = FALSE;
+						if(pGrounded == FALSE) pDir = 2, pRot.x = DX_PI_F*2, pReverse = FALSE;
 					}
+
+					//Camera
+					Camera.z = Player.z + cos(cRot.y)*40;
+					Camera.x = Player.x + sin(cRot.y)*40;
+					if(CheckHitKey(KEY_INPUT_J) == 1) cRot.y -= ROTATE_SPEED;
+					if(CheckHitKey(KEY_INPUT_K) == 1) cRot.y += ROTATE_SPEED;
+					
+					S.x = Player.x - Camera.x;
+					S.y = Player.y - Camera.y; 
+					S.z = Player.z - Camera.z;
+
+					angleH = atan2f(S.x,S.z);
+					//angleV = atan2f(S.x,S.y);
+
+					angleV = ((atan2f(Camera.y,Player.y)) * 0.25) - .39;
+					tempY = atan2f(S.z,S.y);
 				}
 				
 				//Simple Jump
 				if(pJump == TRUE && pGrounded == FALSE)
 				{
-					playerY = anchorY + sin(DX_PI_F*2/90*pPace)*17;
-					pPace++, rotX = DX_PI_F*2;
-					if(playerY <= 0.3f && pPace > 4) pJump = FALSE, pGrounded = TRUE, playerY = 0.0f;
+					Player.y = anchorY + sin(DX_PI_F*2/90*pPace)*17;
+					pPace++, pRot.x = DX_PI_F*2;
+					if(Player.y <= 0.3f && pPace > 4) pJump = FALSE, pGrounded = TRUE, Player.y = 0.0f;
 				}
 
 				//3DAnimation Handler
@@ -131,18 +155,22 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 					MV1SetAttachAnimTime(ModelH,AttachIndex,PlayTime); 
 				}
 
-				//Update ModelH position/rotation
-				MV1SetPosition(ModelH,VGet(playerX,playerY,playerZ));
-				MV1SetRotationXYZ(ModelH,VGet(rotX,rotY,0.0f));
+				//Update ModelH position/pRotation
+				MV1SetPosition(ModelH,VGet(Player.x,Player.y,Player.z));
+				MV1SetRotationXYZ(ModelH,VGet(pRot.x,pRot.y,pRot.z));
 
 				//Update Camera
-				//SetCameraPositionAndTarget_UpVecY(VGet(cameraX, cameraY, cameraZ), VGet(targetX, targetY, targetZ));
+				SetCameraPositionAndAngle(Camera,angleV,angleH,0.0f);
 			}
 
 			//Draw
 			{
 				MV1DrawModel(ModelH);
-				DrawFormatString(0,20,GetColor(255,255,255),"x=%.1f y=%.1f z=%.1f",playerX,playerY,playerZ);
+				MV1DrawModel(SkyH);
+				MV1DrawModel(EnvH1);
+				//MV1DrawModel(EnvH2);
+				DrawFormatString(0,20,GetColor(255,255,255),"x=%.1f y=%.1f z=%.1f",Player.x,Player.y,Player.z);
+				DrawFormatString(0,40,GetColor(255,255,255),"angleV=%.2f, angleH=%.2f",angleV, angleH);
 			}
 		}
 		ScreenFlip();
@@ -161,7 +189,7 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	return 0;					
 }
 
-void rotate(float &x, float &y, const float angle, const float tX, const float tY)
+void pRotate(float &x, float &y, const float angle, const float tX, const float tY)
 {
 	float ox = x-tX;
 	float oy = y-tY;
