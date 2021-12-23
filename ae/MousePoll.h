c6f1abbe3769c6bb258d070f5f6fddd8ba97a911
Torie <38877217+Torietron@users.noreply.github.com>
2021-12-23 03:42:35 -0500

    added mouse controls
    Signed-off-by: Torie <38877217+Torietron@users.noreply.github.com>
    Signed-off-by: Torie <Torietron@gmail.com>

--- ae/MousePoll.h
+++ ae/MousePoll.h
@@ -8,7 +8,15 @@ class MousePoll {
 		int Poll[4];
 		int_fast16_t x, y;
 		MousePoll();
-		int Update();
+		int Update(uint_fast16_t rate = 5);
+		bool Moved();
+		int_fast16_t GetDeltaX();
+		int_fast16_t GetDeltaY();
+		void Reset(int_fast16_t a, int_fast16_t b);
+	private:
+		int_fast16_t last_x, last_y;
+		int_fast16_t delta_x, delta_y;
+		int_fast16_t distance_x, distance_y;
 };
 
 #endif
