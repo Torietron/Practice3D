351fd85aafc7ec8d88661be4672055b1a1bc743a
Torie <38877217+Torietron@users.noreply.github.com>
2021-12-29 02:37:05 -0500

    building modeldata class
    Signed-off-by: Torie <38877217+Torietron@users.noreply.github.com>
    Signed-off-by: Torie <Torietron@gmail.com>

--- ae/ModelData.cpp
+++ ae/ModelData.cpp
@@ -1,2 +1,52 @@
 #include "DxLib.h"
+#include <cstdint>
+#include "ModelData.h"
 
+ModelData::ModelData(float rate)
+:play_rate(rate)
+{
+    //code
+}
+
+void ModelData::Update(MMD_t &m)
+{
+    //Check and Update
+    if(m.AnimSet != m.AnimIndex)
+    {
+        MV1DetachAnim(m.ModelH,m.AttachIndex);
+        m.AttachIndex = MV1AttachAnim(m.ModelH, m.AnimIndex, -1, FALSE);
+        m.TotalTime = MV1GetAttachAnimTotalTime(m.ModelH,m.AttachIndex);
+        m.PlayTime = 0.0f;
+        m.AnimSet = m.AnimIndex; 
+    }
+
+    Animate(m);
+}
+
+void ModelData::SetPlayRate(float a)
+{
+    play_rate = a;
+}
+
+float ModelData::GetPlayRate()
+{
+    return play_rate;
+}
+
+void ModelData::Animate(MMD_t &m)
+{
+    //Advance through the animation
+    if(m.Reverse)
+    {
+        m.PlayTime -= (play_rate - m.PlayOffset);
+        if(m.PlayTime <= 0) m.PlayTime = m.TotalTime;
+    }
+    else
+    {
+        m.PlayTime += play_rate;
+        if(m.PlayTime >= m.TotalTime) m.PlayTime = 0.0f;
+    }
+
+    //Update the playback time point
+    MV1SetAttachAnimTime(m.ModelH,m.AttachIndex,m.PlayTime); 
+}
--- ae/ModelData.h
+++ ae/ModelData.h
@@ -2,21 +2,26 @@
 #ifndef DEF_MODELDATA_H
 #define DEF_MODELDATA_H
 
+#include "DxLib.h"
 #include <cstdint>
-
-class ModelData {
-    public:
-        void Update();
-        float AngleH();
-        float AngleV():
-    private:
-        void Advance();
-};
 
 typedef struct {
     int ModelH, AttachIndex;
     uint_fast8_t AnimSet, AnimIndex;
-    float PlayTime;
+    float PlayTime, TotalTime, PlayOffset;
+    VECTOR Pos, Rot;
+    bool Flag, Reverse;
 } MMD_t;
+
+class ModelData {
+    public:
+        ModelData(float rate = 0.39f);
+        void Update(MMD_t &m);
+        void SetPlayRate(float a);
+        float GetPlayRate();
+    private:
+        float play_rate;
+        void Animate(MMD_t &m);
+};
 
 #endif
